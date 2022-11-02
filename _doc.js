ae22a670d61efa7a516c7855189ad53633c8141b
sorwartalukder <mdsorwar4039@gmail.com>
2022-11-02 16:43:53 +0600

    start crud operation

---
+++ _doc.js
@@ -0,0 +1,18 @@
+/*
+create server road
+1. create project folder 
+2. npm init -y
+3. npm install express
+4. npm install cors
+5. npm install mongodb
+shortcut (npm install express cors mongodb)
+6. npm install dotenv
+7. git init
+8. .gitignore(node_modules)
+9. "scripts": {
+     "start": "node index.js",
+10. create .env file
+11. add .gitignore(.env)
+12. create mongoDb user
+13. start mongoDb crud operation
+*/
