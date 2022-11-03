46fae92ec1f1bcaa0cb81cb21042564118779a02
sorwartalukder <mdsorwar4039@gmail.com>
2022-11-03 18:06:44 +0600

    sen

--- _doc.js
+++ _doc.js
@@ -1,5 +1,5 @@
 /*
-create server road
+create server road map
 1. create project folder 
 2. npm init -y
 3. npm install express
@@ -15,4 +15,13 @@ shortcut (npm install express cors mongodb)
 11. add .gitignore(.env)
 12. create mongoDb user
 13. start mongoDb crud operation
+    crud operation client link: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch\
+    crud operation server link: https://www.mongodb.com/docs/drivers/node/current/usage-examples/
+
+*/
+
+/*
+mongodb problem
+create json data 67-2 time 12:49
+error-> user is not allowed to do action [find] 67-3 time 02:19
 */
