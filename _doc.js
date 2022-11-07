e8e28d4f095a316d4bdb1328802a974b555af285
sorwartalukder <mdsorwar4039@gmail.com>
2022-11-07 18:52:45 +0600

    module 69

--- _doc.js
+++ _doc.js
@@ -25,3 +25,66 @@ mongodb problem
 create json data 67-2 time 12:49
 error-> user is not allowed to do action [find] 67-3 time 02:19
 */
+
+/*
+jot token->
+npm install jsonwebtoken; (sever)
+
+const jwt = require('jsonwebtoken'); (index.js file )
+ctrl+j -> node (enter)
+require('crypto').randomBytes(64).toString('hex')
+ACCESS_TOKEN_SECRET=require('crypto').randomBytes(64).toString('hex'); (.env)
+send user client (login) to server 
+send access token sever to client;
+send headers (authorization: access token) 
+
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' });
+        }
+        req.decoded = decoded;
+        next()
+    });
+}
+
+ const decoded = req.decoded;
+            if (decoded.email !== req.query.email) {
+                res.status(401).send({ message: 'unauthorized access email' })
+            }
+
+.then(res => {
+                if (res.status === 401 || res.status === 403) {
+                    logOut()
+                }
+                return res.json()
+            })            
+
+*/
+
+
+/*
+add vercel config
+add:"build": "node index.js", in package.json file
+vercel
+[
+C:\projects\genius-car-server>vercel
+Vercel CLI 28.4.12
+? Set up and deploy “C:\projects\genius-car-server”? [Y/n] y
+? Which scope do you want to deploy to? sorwartalukder
+? Link to existing project? [y/N] n
+? What’s your project’s name? genius-car-server
+? In which directory is your code located? ./
+]
+
+https://vercel.com/sorwartalukder
+select project
+setting
+add: Environment Variables
+*/
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
@@ -10,14 +11,37 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
+// require('crypto').randomBytes(64).toString('hex')
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.89tmjbq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' });
+        }
+        req.decoded = decoded;
+        next()
+    });
+}
+
 
 async function run() {
     try {
         const serviceCollection = client.db('geniusCar').collection('services');
         const orderCollection = client.db('geniusCar').collection('orders')
+
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            res.send({ token })
+        })
+
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -30,7 +54,11 @@ async function run() {
             const service = await serviceCollection.findOne(query);
             res.send(service)
         })
-        app.get('/orders', async (req, res) => {
+        app.get('/orders', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            if (decoded.email !== req.query.email) {
+                res.status(401).send({ message: 'unauthorized access email' })
+            }
             let query = {};
             if (req.query.email) {
                 query = { email: req.query.email }
@@ -40,12 +68,12 @@ async function run() {
             res.send(orders)
         })
 
-        app.post('/orders', async (req, res) => {
+        app.post('/orders', verifyJWT, async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result)
         })
-        app.patch('/orders/:id', async (req, res) => {
+        app.patch('/orders/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const status = req.body.status
             const query = { _id: ObjectId(id) }
@@ -57,7 +85,7 @@ async function run() {
             const result = await orderCollection.updateOne(query, updatedDoc);
             res.send(result);
         })
-        app.delete('/orders/:id', async (req, res) => {
+        app.delete('/orders/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await orderCollection.deleteOne(query);
